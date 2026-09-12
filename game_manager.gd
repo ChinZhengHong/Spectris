c66d63fd1dbf2b50589f22a4e665242a5df1ceb3
ChinZhengHong <hong99963@gmail.com>
2026-09-12 15:54:19 +0800

    GameManager script for game logic, check the color and record the color be cleared

---
+++ game_manager.gd
@@ -0,0 +1,25 @@
+extends Node
+
+const TOTAL_SPECTRUM_COLORS = 7
+const LINES_TO_UNLOCK = 3
+
+var unlocked_color_index = 0
+var lines_cleared_for_current_color = 0
+
+
+func get_current_target_color() -> int:
+	return unlocked_color_index
+
+func _check_unlock_next_color():
+	if lines_cleared_for_current_color >= LINES_TO_UNLOCK:
+		if unlocked_color_index < TOTAL_SPECTRUM_COLORS - 1:
+			unlocked_color_index += 1
+			lines_cleared_for_current_color = 0
+			print("Unlocked color: ", unlocked_color_index)
+			
+func register_line_clear(cleared_color_ids: Array):
+	for color_id in cleared_color_ids:
+		if color_id == unlocked_color_index:
+			lines_cleared_for_current_color += 1
+
+	_check_unlock_next_color()

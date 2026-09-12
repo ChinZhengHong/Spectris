7942c47daa090c73ff578c661f9044c74c2a8099
ChinZhengHong <hong99963@gmail.com>
2026-09-12 18:03:44 +0800

    faster fall when unlock color and also longer time playing‘

--- game_manager.gd
+++ game_manager.gd
@@ -1,14 +1,18 @@
 extends Node
 
 const TOTAL_SPECTRUM_COLORS = 7
-const LINES_TO_UNLOCK = 3
+const LINES_TO_UNLOCK = [10, 20, 30, 40, 50, 50, 50]
 const BASE_FALL_TIME = 1.0
 const FALL_TIME_DECREASE = 0.1
 const MIN_FALL_TIME = 0.2
+const TIME_DECREASE_INTERVAL = 30.0
+const TIME_DECREASE_AMOUNT = 0.05
+var elapsed_time = 0.0
 const SCORE_PER_WHITE_BLOCK = 50
 var score_since_last_white = 0
 @onready var score_label = get_node("../UI/ScoreLabel")
 @onready var color_label = get_node("../UI/ColorLabel")
+@onready var progress_label = get_node("../UI/ProgressLabel")
 var score = 0
 var unlocked_color_index = 0
 var lines_cleared_for_current_color = 0
@@ -18,7 +22,8 @@ func get_current_target_color() -> int:
 	return unlocked_color_index
 
 func _check_unlock_next_color():
-	if lines_cleared_for_current_color >= LINES_TO_UNLOCK:
+	var threshold = LINES_TO_UNLOCK[unlocked_color_index]
+	if lines_cleared_for_current_color >= threshold:
 		if unlocked_color_index < TOTAL_SPECTRUM_COLORS - 1:
 			unlocked_color_index += 1
 			lines_cleared_for_current_color = 0
@@ -34,7 +39,9 @@ func should_spawn_white_block() -> bool:
 	return false
 
 func get_current_fall_time() -> float:
-	var fall_time = BASE_FALL_TIME - (unlocked_color_index * FALL_TIME_DECREASE)
+	var color_reduction = unlocked_color_index * FALL_TIME_DECREASE
+	var time_reduction = floor(elapsed_time / TIME_DECREASE_INTERVAL) * TIME_DECREASE_AMOUNT
+	var fall_time = BASE_FALL_TIME - color_reduction - time_reduction
 	return max(fall_time, MIN_FALL_TIME)
 	
 func register_line_clear(cleared_color_ids: Array):
@@ -50,8 +57,16 @@ func register_line_clear(cleared_color_ids: Array):
 	
 func _update_ui():
 	score_label.text = "Score: " + str(score)
-	var color_names = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
+	var color_names = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Purple"]
 	color_label.text = "Color: " + color_names[unlocked_color_index]
-
+	if unlocked_color_index < TOTAL_SPECTRUM_COLORS - 1:
+		var threshold = LINES_TO_UNLOCK[unlocked_color_index]
+		progress_label.text = "Progress: " + str(lines_cleared_for_current_color) + "/" + str(threshold)
+	else:
+		progress_label.text = "Max color reached!"
+		
 func _ready():
 	_update_ui()
+	
+func _process(delta):
+	elapsed_time += delta

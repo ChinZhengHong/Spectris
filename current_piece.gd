fc6d0d9cb27d6a1558b11ef70e6f97a005b09e9e
ChinZhengHong <hong99963@gmail.com>
2026-09-12 15:26:10 +0800

    game over when block touch upper, and fix the shape O, so it can't spin since it already 2x2

--- current_piece.gd
+++ current_piece.gd
@@ -8,6 +8,7 @@ const CELL_SIZE = 32
 var current_type = PieceData.PieceType.T
 var grid_position = Vector2i(4, 0)
 var block_offsets = []
+var is_game_over = false
 @onready var board = get_node("../Board")
 
 func _can_move(offsets: Array, pos: Vector2i) -> bool:
@@ -15,9 +16,9 @@ func _can_move(offsets: Array, pos: Vector2i) -> bool:
 		var cell = pos + offset
 		if cell.x < 0 or cell.x >= board.GRID_WIDTH:
 			return false
-		if cell.y < 0 or cell.y >= board.GRID_HEIGHT:
+		if cell.y >= board.GRID_HEIGHT:
 			return false
-		if board.grid[cell.y][cell.x] != -1:
+		if cell.y >= 0 and board.grid[cell.y][cell.x] != -1:
 			return false
 	return true
 
@@ -36,6 +37,12 @@ func _spawn_piece():
 	current_type = types[randi() % types.size()]
 	block_offsets = PieceData.SHAPES[current_type]
 	grid_position = Vector2i(4, 0)
+
+	if not _can_move(block_offsets, grid_position):
+		is_game_over = true
+		print("Game Over")
+		return
+
 	queue_redraw()
 	
 func _draw():
@@ -46,6 +53,8 @@ func _draw():
 		
 # input
 func _unhandled_input(event):
+	if is_game_over:
+		return
 	if event.is_action_pressed("ui_left"):
 		var new_pos = grid_position + Vector2i(-1, 0)
 		if _can_move(block_offsets, new_pos):
@@ -63,13 +72,16 @@ func _unhandled_input(event):
 			queue_redraw()
 			
 	elif event.is_action_pressed("ui_up"):
-		var new_offsets = _get_rotated_offsets(block_offsets)
-		if _can_move(new_offsets, grid_position):
-			block_offsets = new_offsets
-			queue_redraw()
+		if current_type != PieceData.PieceType.O:
+			var new_offsets = _get_rotated_offsets(block_offsets)
+			if _can_move(new_offsets, grid_position):
+				block_offsets = new_offsets
+				queue_redraw()
 
 
 func _on_timer_timeout() -> void:
+	if is_game_over:
+		return
 	var new_pos = grid_position + Vector2i(0, 1)
 	if _can_move(block_offsets, new_pos):
 		grid_position = new_pos

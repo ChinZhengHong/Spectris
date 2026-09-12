0a16e9a2fd22c7bd0e0d0bd2330d1ec4a0c9c14e
ChinZhengHong <hong99963@gmail.com>
2026-09-12 11:29:33 +0800

    add main scene and board atttached to script

---
+++ board.gd
@@ -0,0 +1,13 @@
+extends Node2D
+const GRID_WIDTH = 10
+const GRID_HEIGHT = 20
+const CELL_SIZE = 32
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

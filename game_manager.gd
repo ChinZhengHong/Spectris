9ecea7212931e70f21c13f94a8f1c664de83755c
ChinZhengHong <hong99963@gmail.com>
2026-09-13 10:54:23 +0800

    add text for control and main menu button when game over

--- game_manager.gd
+++ game_manager.gd
@@ -104,3 +104,8 @@ func _play_clear_row_sound():
 
 func _on_restart_button_pressed() -> void:
 	get_tree().reload_current_scene()
+
+
+func _on_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+	
